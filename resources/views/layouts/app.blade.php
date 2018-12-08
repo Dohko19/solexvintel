f4a8ef0c97dbca0d974a43e1b363b0ff9c9247e3
Daniel <43178677+Dohko19@users.noreply.github.com>
2018-12-07 20:48:10 -0600

    Initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Solexvintel - Pagina Oficial</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="RanGO Project">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="styles/bootstrap4/bootstrap.min.css">
+<link href="plugins/fontawesome-free-5.0.1/css/fontawesome-all.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/animate.css">
+<link rel="stylesheet" type="text/css" href="plugins/slick-1.8.0/slick.css">
+<link href="plugins/icon-font/styles.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="styles/main_styles.css">
+<link rel="stylesheet" type="text/css" href="styles/responsive.css">
+</head>
+<body>
+	@yield('contenido')
+<!-- Footer Services -->
+				<div class="col-lg-2">
+
+					<div class="footer_col">
+						<div class="footer_col_title">Services</div>
+						<ul>
+							<li><a href="#">Social media</a></li>
+							<li><a href="#">Management</a></li>
+							<li><a href="#">Branding</a></li>
+						</ul>
+					</div>
+					
+					<div class="footer_col">
+						<div class="footer_col_title">Aditionals</div>
+						<ul>
+							<li><a href="#">Social media</a></li>
+							<li><a href="#">Management</a></li>
+							<li><a href="#">Branding</a></li>
+						</ul>
+					</div>
+
+				</div>
+
+				<!-- Footer Menu -->
+				<div class="col-lg-2">
+
+					<div class="footer_col">
+						<div class="footer_col_title">Menu</div>
+						<ul>
+							<li><a href="#">Home</a></li>
+							<li><a href="#">About us</a></li>
+							<li><a href="#">Services</a></li>
+							<li><a href="#">Portfolio</a></li>
+							<li><a href="#">Blog</a></li>
+							<li><a href="#">Contact</a></li>
+							<li><a href="#">Testimonials</a></li>
+						</ul>
+					</div>
+
+				</div>
+
+				<!-- Footer About -->
+				<div class="col-lg-2">
+
+					<div class="footer_col">
+						<div class="footer_col_title">About us</div>
+						<ul>
+							<li><a href="#">The team</a></li>
+							<li><a href="#">History</a></li>
+							<li><a href="#">Company</a></li>
+							<li><a href="#">Support</a></li>
+						</ul>
+					</div>
+
+				</div>
+
+				<!-- Footer Community -->
+				<div class="col-lg-2">
+
+					<div class="footer_col">
+						<div class="footer_col_title">Community</div>
+						<ul>
+							<li><a href="#">Blog</a></li>
+							<li><a href="#">Forums</a></li>
+							<li><a href="#">Q&A</a></li>
+							<li><a href="#">Purposes</a></li>
+						</ul>
+					</div>
+
+				</div>
+
+			</div>
+
+			<div class="row">
+				<div class="col">
+					<!-- Copyright -->
+					<div class="footer_cr_2">2017 All rights reserved</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+</div>
+
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="styles/bootstrap4/popper.js"></script>
+<script src="styles/bootstrap4/bootstrap.min.js"></script>
+<script src="plugins/greensock/TweenMax.min.js"></script>
+<script src="plugins/greensock/TimelineMax.min.js"></script>
+<script src="plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="plugins/greensock/animation.gsap.min.js"></script>
+<script src="plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="plugins/slick-1.8.0/slick.js"></script>
+<script src="plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="plugins/scrollTo/jquery.scrollTo.min.js"></script>
+<script src="plugins/easing/easing.js"></script>
+<script src="js/custom.js"></script>
+</body>
+</html>
